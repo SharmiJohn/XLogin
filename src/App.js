dcf0bf6d6319cf1e2a40892c16bd80dfa8ac458a
SharmiJohn <1422000joselvi@gmail.com>
2024-07-03 13:11:41 +0530

    fifth

--- src/App.js
+++ src/App.js
@@ -3,19 +3,19 @@ import { useState } from "react";
 function App() {
   const [username,setUsername]=useState("");
   const[password,setPassword]=useState("");
-  const[data,setData]=useState({Username:"user",Password:"password"});
+ 
   const[result,setResult]=useState(false);
  const [error,setError]=useState("");
   const handleSubmit=(e)=>{
     e.preventDefault();
-      if(username===data.Username && password===data.Password){
+      if(username==="user" && password==="password"){
         setError("");
         setResult(true);
 
       }
       else{
         setError("Invalid username or password")
-         setResult(false)
+         setResult(false);
 
       }
      
