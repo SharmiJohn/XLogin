3f3b8170cc8e549820c32f31c5e1db6204fe8b42
SharmiJohn <1422000joselvi@gmail.com>
2024-07-03 13:50:45 +0530

    close

--- src/App.js
+++ src/App.js
@@ -24,13 +24,18 @@ function App() {
   return (
     <div className="App" style={{display:"flex",flexDirection:"column",alignItems:"flex-start",justifyContent:"center"}}>
       <h1>Login Page</h1>
-      {result===true&&(<h3>Welcome,user!</h3>)}
+      {result===true&&(<h3>Welcome,{username}!</h3>)}
       {result===false && 
-      <form onSubmit={(e)=>handleSubmit(e) } style={{display:"flex" ,alignItems:"flex-start",justifyContent:"center"}}>
+      <form onSubmit={(e)=>handleSubmit(e) } style={{display:"flex" , flexDirection:"column",alignItems:"flex-start",justifyContent:"center"}}>
         {error.length!==0 && <p>{error}</p>}
+        <div style={{display:"flex"}}>
         <label htmlFor="username">UserName: </label>
         <input type="text" value={username} placeholder="username" onChange={(e)=>setUsername(e.target.value)} required/> 
-        <label htmlFor="password">Password: </label> <input  type="password" value={password} placeholder="password" onChange={(e)=>setPassword(e.target.value)} required/>
+        </div>
+       <div style={{display:"flex"}}>
+       <label htmlFor="password">Password: </label> <input  type="password" value={password} placeholder="password" onChange={(e)=>setPassword(e.target.value)} required/>
+       </div>
+       
        
         <button type="submit">Submit</button>
       </form>}
